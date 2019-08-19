2f177c3945bf9f16a895c8cd198afaf523fa2ed4
roger <roger.cano@dotz.com>
2019-08-19 15:50:23 -0300

    este

--- src/org/somecompany/GitRepository.groovy
+++ src/org/somecompany/GitRepository.groovy
@@ -3,17 +3,12 @@ package org.somecompany
 class GitRepository {
     GitRepository(String _repository) {
         this.repository = _repository;
-        setName(_repository);
     }
 
     String repository;
     String name;
 
-    public void setName(String _repository){
-        name = _repository.tokenize('/')[-1]
-    }
-
     public String getName(){
-        return name;
+        return repository.tokenize('/')[-1];
     }
 }
