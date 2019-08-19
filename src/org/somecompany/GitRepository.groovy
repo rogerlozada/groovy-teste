024f99b3ca98bf849dbaa3f9396cf31b67790302
roger <roger.cano@dotz.com>
2019-08-19 15:33:18 -0300

    este

--- src/org/somecompany/GitRepository.groovy
+++ src/org/somecompany/GitRepository.groovy
@@ -3,25 +3,12 @@ package org.somecompany
 import com.cloudbees.groovy.cps.NonCPS
 
 class GitRepository {
-    //GitRepository(String _repository) {
-     //   this.repository = _repository;
-   // }
-
-
-    private String repository;
-    private String name;
-
-    //@NonCPS
-    def setName(_repository)
-    {
-        this.name = _repository.tokenize('/')[-1]
+    GitRepository(String _repository) {
+        this.repository = _repository;
     }
 
-    def getRepository() {
-        return this.repository;
-    }
 
-    def getName() {
-        return this.name;
-    }
+    String repository;
+    String name;
+
 }
